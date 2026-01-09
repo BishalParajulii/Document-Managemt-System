83d3a75095e4301c1f806f00fc5cad61b1993266
Bishal Parajuli <parajulibishal00@gmail.com>
2026-01-09 09:30:50 +0545

    working on passwords

--- dms_project/documents/serializers.py
+++ dms_project/documents/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
 from .models import Document , DocumentContent
+from django.contrib.auth.hashers import make_password
 
 class DocumentSerializer(serializers.ModelSerializer):
     created_by = serializers.CharField(source="created_by.username" , read_only = True)
+    password = serializers.CharField(write_only=True, required=False , allow_blank = True)
+    is_locked = serializers.BooleanField(read_only=True)
+    
     class Meta:
         model = Document
-        fields = ['id' , 'title' , 'file' , 'created_at' , 'created_by']
-        read_only_fields = ['id' , 'created_at']
+        fields = ['id' , 'title' , 'file' , 'created_at' , 'created_by' , 'password' , 'is_locked']
+        read_only_fields = ['id' , 'created_at' , 'created_by' , 'is_locked']
+        
+    def create(self, validated_data):
+        password = validated_data.pop("password" , None)
+        doc = Document.objects.create(**validated_data)
+        
+        if password:
+            doc.locked_password = make_password(password)
+            doc.save()
+            
+        return doc
         
         
 class DocumentContentSerializer(serializers.ModelSerializer):
